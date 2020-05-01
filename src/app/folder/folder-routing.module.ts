13dbdf403055da3d884cc6ee1f5f509fffa61d2c
velrino <denismagalhaesvelrino@gmail.com>
2020-05-01 04:56:34 -0300

    :zap: imported HeaderComponent

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FolderPage } from './folder.page';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,8 @@ const routes: Routes = [
     component: FolderPage
   }
 ];
+
+export const RoutingComponents = [HeaderComponent]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
