f0b1c7cbdb010c31741ed14e1a6741056ddfea37
velrino <denismagalhaesvelrino@gmail.com>
2020-05-01 04:36:20 -0300

    :art: extends BaseComponent and switch darkMode with click using EventEmitter

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { BaseComponent } from 'src/app/shared/components/base.components'
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
-export class FolderPage implements OnInit {
+export class FolderPage extends BaseComponent implements OnInit {
+  public component = {
+    darkMode: true
+  }
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) {
+    super()
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.switchDarkMode();
+  }
+
+  switchDarkMode() {
+    this.eventEmitter({ func: 'darkMode', param: this.component.darkMode });
   }
 
 }
